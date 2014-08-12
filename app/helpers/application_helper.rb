975a319db1b08f7a40751960906afe12fa549689
Marcelo Goncalves <marcelocg@gmail.com>
2014-08-12 03:30:00 -0300

    Improved trace data visualization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def execution_class(exec)
+    css_class =  (exec[:executed] ? "executed" : "predicted")
+    css_class << (exec[:met_sla]  ? " met_sla" : " not_met_sla")
+    css_class      
+  end
 end
